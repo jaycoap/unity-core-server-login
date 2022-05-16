464289a8f96d81c1ef4ab3812c730b25af0e9887
jaycoap <rooqoxo@gmail.com>
2022-05-16 23:57:26 +0900

    데이터베이스 정보 업데이트

--- sql/tb_level_info.sql
+++ sql/tb_level_info.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `tb_level_info` (
   `level` int(11) NOT NULL,
-  `exp` int(11) DEFAULT NULL,
-  `str` int(11) DEFAULT NULL,
-  `int` int(11) DEFAULT NULL,
-  `fit` int(11) DEFAULT NULL,
+  `experience` int(11) DEFAULT NULL,
+  `defense` int(11) DEFAULT NULL,
+  `health` int(11) DEFAULT NULL,
+  `damage` int(11) DEFAULT NULL,
+  `speed` int(11) DEFAULT NULL,
   PRIMARY KEY (`level`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
