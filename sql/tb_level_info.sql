f9f9ad7586b2af14b53b86baf4f9eb43bc7da141
jaycoap <rooqoxo@gmail.com>
2022-05-16 15:26:21 +0900

    2022-05-16 PM 15:24
    모델 재설정, 런치 재설정,sql 재설정 서버 db연동 완료

--- sql/tb_level_info.sql
+++ sql/tb_level_info.sql
@@ -1,9 +1,8 @@
 CREATE TABLE `tb_level_info` (
   `level` int(11) NOT NULL,
-  `experience` int(11) DEFAULT NULL,
-  `defense` int(11) DEFAULT NULL,
-  `health` int(11) DEFAULT NULL,
-  `damage` int(11) DEFAULT NULL,
-  `speed` int(11) DEFAULT NULL,
+  `exp` int(11) DEFAULT NULL,
+  `str` int(11) DEFAULT NULL,
+  `int` int(11) DEFAULT NULL,
+  `fit` int(11) DEFAULT NULL,
   PRIMARY KEY (`level`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- sql/tb_upgrade_info.sql
+++ sql/tb_upgrade_info.sql
@@ -1,8 +1,6 @@
 CREATE TABLE `tb_upgrade_info` (
   `upgrade_id` int(11) NOT NULL,
-  `upgrade_type` varchar(100) DEFAULT NULL,
-  `upgrade_level` int(11) DEFAULT NULL,
-  `upgrade_amount` int(11) DEFAULT NULL,
-  `upgrade_cost` int(11) DEFAULT NULL,
+  `upgrade_level` int(11) DEFAULT '1',
+  `upgrade_appoint` int(11) DEFAULT NULL,
   PRIMARY KEY (`upgrade_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
